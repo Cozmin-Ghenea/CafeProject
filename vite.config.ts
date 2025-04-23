9d3ca1fbc0fb0e43f02d20b1790369e629d81eab
Cozmin <cozmin.ghenea@gmail.com>
2025-04-23 13:14:28 +0300

    photos

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import path from "path";
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 export default defineConfig({
   plugins: [react()],
   base: "/CafeProject/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
